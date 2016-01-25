6c53a46ce6de926ac6ba4e282a570322896785a9
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-01-25 15:42:57 +0100

    Added fabric

---
+++ app/src/main/java/com/appunite/shoutit/App.java
@@ -0,0 +1,23 @@
+package com.appunite.shoutit;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initFabric();
+    }
+
+    private void initFabric() {
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
+    }
+}
