2ca75a216ae6a59a5a476e07286c3554dbe7c88a
Piotr Krystyniak <piotr.krystyniak@appunite.com>
2016-02-03 18:33:56 +0100

    Change upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,6 @@
 set -e -x
-if [ ${CI_BUILD_REF_NAME} == "master" ] || [ ${CI_BUILD_REF_NAME} == "develop" ];
-then
-	./gradlew --parallel \
-	--stacktrace \
-	--project-prop commitId="${CI_BUILD_REF}" \
-	--project-prop versionSuffix="$CI_BUILD_REF_NAME.$CI_BUILD_ID" \
-	crashlyticsUploadDistributionProdRelease crashlyticsUploadDistributionProdStaging
-fi
+./gradlew --parallel \
+--stacktrace \
+--project-prop commitId="${CI_BUILD_REF}" \
+--project-prop versionSuffix="$CI_BUILD_REF_NAME.$CI_BUILD_ID" \
+crashlyticsUploadDistributionProdRelease crashlyticsUploadDistributionProdStaging
