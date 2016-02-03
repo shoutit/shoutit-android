8d38e49ece54be996d3c7f6305ec022d5e9fdb57
Piotr Krystyniak <piotr.krystyniak@appunite.com>
2016-02-03 19:01:14 +0100

    Add guest flag

--- app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
+++ app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        if (!mUserPreferences.isUserLoggedIn()) {
+        if (!mUserPreferences.isUserLoggedIn() && !mUserPreferences.isGuest()) {
             finish();
             startActivity(IntroActivity.newIntent(this));
         }
