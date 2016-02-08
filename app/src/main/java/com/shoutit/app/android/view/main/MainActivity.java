d3322b78f3f38b7b8f3630eb156fe20a60b53d2f
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-02-08 13:10:35 +0100

    fix

--- app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
+++ app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
@@ -21,6 +21,7 @@ import com.shoutit.app.android.UserPreferences;
 import com.shoutit.app.android.dagger.ActivityModule;
 import com.shoutit.app.android.dagger.BaseActivityComponent;
 import com.shoutit.app.android.view.home.HomeFragment;
+import com.shoutit.app.android.view.intro.IntroActivity;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
