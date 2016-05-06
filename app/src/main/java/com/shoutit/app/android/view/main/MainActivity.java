ede80f529c7be02a46eca44ee2642b59836c1e15
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-05-06 17:12:50 +0200

    Added bindTolfiecycle to stats subscription

--- app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
+++ app/src/main/java/com/shoutit/app/android/view/main/MainActivity.java
@@ -136,6 +136,7 @@ public class MainActivity extends BaseActivity implements OnMenuItemSelectedList
 
     private void subscribeToStats() {
         mStatsSubscription.add(mPusherHelper.getStatsObservable()
+                .compose(this.<Stats>bindToLifecycle())
                 .subscribe(new Action1<Stats>() {
                     @Override
                     public void call(Stats pusherStats) {
@@ -145,6 +146,7 @@ public class MainActivity extends BaseActivity implements OnMenuItemSelectedList
         mStatsSubscription.add(mUserPreferences.getUserObservable()
                 .filter(Functions1.isNotNull())
                 .distinctUntilChanged()
+                .compose(this.<User>bindToLifecycle())
                 .subscribe(new Action1<User>() {
                     @Override
                     public void call(User user) {
