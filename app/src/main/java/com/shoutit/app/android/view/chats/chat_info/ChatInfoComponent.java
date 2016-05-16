d98a235e3a56ef71c89dd125fe07be30c8ced531
Piotr Krystyniak <piotr.krystyniak@appunite.com>
2016-05-16 14:54:31 +0200

    Chat info

---
+++ app/src/main/java/com/shoutit/app/android/view/chats/chat_info/ChatInfoComponent.java
@@ -0,0 +1,22 @@
+package com.shoutit.app.android.view.chats.chat_info;
+
+import com.shoutit.app.android.dagger.ActivityModule;
+import com.shoutit.app.android.dagger.ActivityScope;
+import com.shoutit.app.android.dagger.AppComponent;
+import com.shoutit.app.android.dagger.BaseActivityComponent;
+
+import dagger.Component;
+
+@ActivityScope
+@Component(
+        dependencies = AppComponent.class,
+        modules = {
+                ActivityModule.class
+        }
+)
+public interface ChatInfoComponent extends BaseActivityComponent {
+
+    void inject(ChatInfoActivity activity);
+
+}
+
