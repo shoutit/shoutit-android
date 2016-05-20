5e032e311cc078d21334803eb011b637a5da6146
Piotr Krystyniak <piotr.krystyniak@appunite.com>
2016-05-20 14:27:46 +0200

    Fix missing import

--- app/src/main/java/com/shoutit/app/android/view/chats/ChatActivity.java
+++ app/src/main/java/com/shoutit/app/android/view/chats/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.shoutit.app.android.view.chats;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
