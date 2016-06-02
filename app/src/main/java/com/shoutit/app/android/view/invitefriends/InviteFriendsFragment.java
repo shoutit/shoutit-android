ad33e1799bf5cc8a1609d87cfbfcf3acb67875f5
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-06-02 15:39:53 +0200

    [#118358321] Twitter share

--- app/src/main/java/com/shoutit/app/android/view/invitefriends/InviteFriendsFragment.java
+++ app/src/main/java/com/shoutit/app/android/view/invitefriends/InviteFriendsFragment.java
@@ -1,20 +1,27 @@
 package com.shoutit.app.android.view.invitefriends;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.common.base.Optional;
 import com.shoutit.app.android.BaseFragment;
 import com.shoutit.app.android.R;
 import com.shoutit.app.android.dagger.BaseActivityComponent;
 import com.shoutit.app.android.dagger.FragmentModule;
+import com.shoutit.app.android.utils.ColoredSnackBar;
 import com.shoutit.app.android.utils.IntentHelper;
 import com.shoutit.app.android.view.invitefriends.contactsfriends.ContactsFriendsActivity;
 import com.shoutit.app.android.view.invitefriends.facebookfriends.FacebookFriendsActivity;
 import com.shoutit.app.android.view.loginintro.FacebookHelper;
+
+import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -69,10 +76,32 @@ public class InviteFriendsFragment extends BaseFragment {
                 FacebookHelper.showAppInviteDialog(this, SHARE_APP_TEXT);
                 break;
             case R.id.invite_friends_invite_twitter:
+                shareThroughTwitter();
                 break;
             case R.id.invite_friends_share_app:
                 startActivity(IntentHelper.getShareIntent(SHARE_APP_TEXT));
                 break;
+        }
+    }
+
+    private void shareThroughTwitter() {
+        final List<Intent> twitterShareIntents = IntentHelper.getTwitterShareIntent(
+                getActivity().getPackageManager(), getString(R.string.invite_text));
+
+        if (!twitterShareIntents.isEmpty()) {
+            if (twitterShareIntents.size() > 1) {
+                final Intent chooser = Intent.createChooser(
+                        twitterShareIntents.get(0), getString(R.string.invite_chooser_text));
+                twitterShareIntents.remove(0);
+                chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, twitterShareIntents.toArray(new Parcelable[]{}));
+                startActivity(chooser);
+            } else {
+                startActivity(twitterShareIntents.get(0));
+            }
+        } else {
+            ColoredSnackBar.error(ColoredSnackBar.contentView(getActivity()),
+                    R.string.invite_no_twitter_app, Snackbar.LENGTH_LONG)
+                    .show();
         }
     }
 
