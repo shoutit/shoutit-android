57727c85944251ac97af0d166d1d77f16eeccf4c
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-06-03 15:13:12 +0200

    [#120748509] Camera permissions improvements

--- app/src/main/java/com/shoutit/app/android/view/media/RecordMediaActivity.java
+++ app/src/main/java/com/shoutit/app/android/view/media/RecordMediaActivity.java
@@ -1,14 +1,13 @@
 package com.shoutit.app.android.view.media;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.widget.Toast;
 
 import com.commonsware.cwac.cam2.AbstractCameraActivity;
@@ -18,13 +17,9 @@ import com.commonsware.cwac.cam2.CameraSelectionCriteria;
 import com.commonsware.cwac.cam2.Facing;
 import com.commonsware.cwac.cam2.FocusMode;
 import com.google.common.base.Preconditions;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.MultiplePermissionsReport;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.shoutit.app.android.R;
-import com.shoutit.app.android.utils.SystemUIUtils;
+import com.shoutit.app.android.utils.ColoredSnackBar;
+import com.shoutit.app.android.utils.PermissionHelper;
 
 import java.util.List;
 
@@ -40,8 +35,9 @@ public class RecordMediaActivity extends AbstractCameraActivity
     private static final String EXTRA_IS_FIRST_MEDIA = "extra_is_first_media";
     private static final String EXTRA_USE_EDITOR = "extra_use_editor";
 
+    private static final int REQUEST_CODE_PERMISSIONS = 1;
+
     private CameraFragment cameraFragment;
-    private MultiplePermissionsListener customMultiplePermissionsListener;
 
     private boolean isActivityForResult;
     private boolean isVideoFirst;
@@ -99,8 +95,6 @@ public class RecordMediaActivity extends AbstractCameraActivity
             useEditor = extras.getBoolean(EXTRA_USE_EDITOR);
         }
 
-        Dexter.continuePendingRequestsIfPossible(customMultiplePermissionsListener);
-
         super.onCreate(savedInstanceState);
     }
 
@@ -117,53 +111,20 @@ public class RecordMediaActivity extends AbstractCameraActivity
             cameraFragment = (CameraFragment) fragment;
         }
 
-        if (cameraFragment != null) {
-            this.initPermissionLiseners();
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (Dexter.isRequestOngoing()) return;
-                Dexter.checkPermissions(customMultiplePermissionsListener, Manifest.permission.CAMERA,
-                        Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            } else {
-                initCamera();
-            }
+        if (cameraFragment != null && PermissionHelper.checkPermissions(
+                this, REQUEST_CODE_PERMISSIONS, ColoredSnackBar.contentView(this),
+                R.string.permission_camera_create_shout_explanation,
+                new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE})) {
+            initCamera();
         }
-    }
-
-    private void initPermissionLiseners() {
-
-        customMultiplePermissionsListener = new MultiplePermissionsListener() {
-            @Override
-            public void onPermissionsChecked(MultiplePermissionsReport report) {
-                if (report.areAllPermissionsGranted()) {
-                    initCamera();
-                } else {
-                    new AlertDialog.Builder(RecordMediaActivity.this).setTitle("denied permission")
-                            .setMessage("denied permission")
-                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                    finish();
-                                }
-                            })
-                            .setIcon(R.mipmap.ic_launcher)
-                            .show();
-                }
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions,
-                                                           PermissionToken token) {
-                token.continuePermissionRequest();
-            }
-        };
     }
 
     @Override
     public void onInitializationFailed(Exception cause) {
-
     }
 
     @Override
@@ -235,4 +196,22 @@ public class RecordMediaActivity extends AbstractCameraActivity
     public void onEventMainThread(CameraController.ControllerDestroyedEvent event) {
         // dont destroy
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            final boolean permissionsGranted = PermissionHelper.arePermissionsGranted(grantResults);
+            if (permissionsGranted) {
+                ColoredSnackBar.success(findViewById(android.R.id.content), R.string.permission_granted, Snackbar.LENGTH_SHORT).show();
+                initCamera();
+            } else {
+                Toast.makeText(this, R.string.permission_not_granted, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
 }
