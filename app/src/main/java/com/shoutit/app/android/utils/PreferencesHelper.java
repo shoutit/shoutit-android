027fa9493e099d802e4e148a3cb7422a81da5513
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-06-06 10:59:50 +0200

    Little improvment

--- app/src/main/java/com/shoutit/app/android/utils/PreferencesHelper.java
+++ app/src/main/java/com/shoutit/app/android/utils/PreferencesHelper.java
@@ -5,20 +5,20 @@ import com.shoutit.app.android.UserPreferences;
 import com.shoutit.app.android.api.model.User;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 public class PreferencesHelper {
 
-    @Nonnull
-    private final UserPreferences preferences;
+    @Nullable
+    private final User currentUser;
 
     @Inject
     public PreferencesHelper(@Nonnull UserPreferences preferences) {
-        this.preferences = preferences;
+        currentUser = preferences.getUser();
     }
 
     public boolean isMyProfile(@Nonnull String userNameToCompare) {
-        final User currentUser = preferences.getUser();
         return currentUser != null &&
                 (userNameToCompare.equals(currentUser.getUsername()) || userNameToCompare.equals(User.ME));
     }
