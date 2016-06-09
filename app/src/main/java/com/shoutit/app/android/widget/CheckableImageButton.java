87c0b933a5fba7a843cf8882564a4cee9c0bb60e
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-06-09 16:02:12 +0200

    Lint fix

--- app/src/main/java/com/shoutit/app/android/widget/CheckableImageButton.java
+++ app/src/main/java/com/shoutit/app/android/widget/CheckableImageButton.java
@@ -1,6 +1,8 @@
 package com.shoutit.app.android.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.ImageButton;
 
@@ -20,6 +22,7 @@ public class CheckableImageButton extends ImageButton {
         super(context, attrs, defStyleAttr);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public CheckableImageButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
