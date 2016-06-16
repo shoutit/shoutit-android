04ef94c66b22c4220468d002211bacd0e00d0972
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-06-16 14:51:21 +0200

    [#121391827] Layout fixes for 4.1 and 4.2

--- app/src/main/java/com/shoutit/app/android/view/media/PublishMediaShoutFragment.java
+++ app/src/main/java/com/shoutit/app/android/view/media/PublishMediaShoutFragment.java
@@ -20,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.appunite.rx.ResponseOrError;
 import com.appunite.rx.android.MyAndroidSchedulers;
@@ -82,7 +83,7 @@ public class PublishMediaShoutFragment extends Fragment {
     @Bind(R.id.camera_progress)
     View progress;
     @Bind(R.id.camera_published_done)
-    Button mCameraPublishedDone;
+    TextView mCameraPublishedDone;
     @Bind(R.id.fragment_camera_close)
     ImageButton closeButton;
     @Bind(R.id.camera_published_facebook_checkbox)
