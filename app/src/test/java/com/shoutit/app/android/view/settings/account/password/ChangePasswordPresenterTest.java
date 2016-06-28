fc51e50717203ed3e4b06bed1c03405834dead74
Piotr Krystyniak <piotr.krystyniak@appunite.com>
2016-06-28 12:26:46 +0200

    Fix tests

--- app/src/test/java/com/shoutit/app/android/view/settings/account/password/ChangePasswordPresenterTest.java
+++ app/src/test/java/com/shoutit/app/android/view/settings/account/password/ChangePasswordPresenterTest.java
@@ -34,9 +34,9 @@ public class ChangePasswordPresenterTest {
         when(apiService.changePassword(any(ChangePasswordRequest.class)))
                 .thenReturn(Observable.just(ResponseBody.create(null, "z")));
 
-        when(userPreferences.getPageOrUserObservable())
-                .thenReturn(Observable.just(new User("z", null, null, null, null, null, null, null, false, null,
-                null, false, false, false, null, 1, null, null, null, 1, null, false, null, null, null, null, null, null, null, null, null)));
+        when(userPreferences.getUser())
+                .thenReturn(new User("z", null, null, null, null, null, null, null, false, null,
+                null, false, false, false, null, 1, null, null, null, 1, null, false, null, null, null, null, null, null, null, null, null));
 
         presenter = new ChangePasswordPresenter(apiService, userPreferences, Schedulers.immediate(), Schedulers.immediate());
     }
