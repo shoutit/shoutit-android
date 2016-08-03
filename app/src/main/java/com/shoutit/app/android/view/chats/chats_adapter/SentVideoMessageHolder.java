64653b898ff47ae87bab6b4dcd02ad295d6aee4d
Marcin Adamczewski <marcin.adamczewski@appunite.com>
2016-08-03 16:26:57 +0200

    [#117698031] Chat views improvements

--- app/src/main/java/com/shoutit/app/android/view/chats/chats_adapter/SentVideoMessageHolder.java
+++ app/src/main/java/com/shoutit/app/android/view/chats/chats_adapter/SentVideoMessageHolder.java
@@ -2,14 +2,16 @@ package com.shoutit.app.android.view.chats.chats_adapter;
 
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.util.LayoutDirection;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.appunite.rx.android.adapter.BaseAdapterItem;
 import com.appunite.rx.android.adapter.ViewHolderManager;
 import com.shoutit.app.android.R;
-import com.shoutit.app.android.view.chats.message_models.ReceivedVideoMessage;
+import com.shoutit.app.android.utils.RtlUtils;
 import com.shoutit.app.android.view.chats.message_models.SentVideoMessage;
 import com.squareup.picasso.Picasso;
 
@@ -28,6 +30,8 @@ public class SentVideoMessageHolder extends ViewHolderManager.BaseViewHolder<Bas
     ImageView mChatsSentVideoImageview;
     @Bind(R.id.chats_sent_video_textview)
     TextView mChatsSentVideoTextview;
+    @Bind(R.id.chat_sent_video_container)
+    ViewGroup container;
 
     public SentVideoMessageHolder(@Nonnull View itemView, Picasso picasso) {
         super(itemView);
@@ -40,18 +44,17 @@ public class SentVideoMessageHolder extends ViewHolderManager.BaseViewHolder<Bas
     public void bind(@Nonnull BaseAdapterItem item) {
         final SentVideoMessage message = (SentVideoMessage) item;
 
+        RtlUtils.setLayoutDirection(container, LayoutDirection.LTR);
+
         mPicasso.load(message.getVideoThumbnail())
                 .fit()
                 .centerCrop()
                 .into(mChatsSentVideoImageview);
+
+        RtlUtils.setLayoutDirection(mChatsSentVideoTextview, LayoutDirection.LOCALE);
         mChatsSentVideoTextview.setText(message.getTime());
 
-        mItemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                message.click();
-            }
-        });
+        mItemView.setOnClickListener(v -> message.click());
     }
 
     public static ViewHolderManager.BaseViewHolder create(@NonNull View view, Picasso picasso) {
